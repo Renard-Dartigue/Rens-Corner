050b72192262636da5087e1a20ed2bfad796337e
Engine <rg.dartigue@gmail.com>
2023-09-04 22:42:32 -0400

    Got all the tabs to work but the Portfolio tag. Keept display an error: Cannot locate /Portfolio. However it is linked. Will move on the something else and comeback to fix the problem later

--- portfolio-project/src/components/Navi.jsx
+++ portfolio-project/src/components/Navi.jsx
@@ -4,42 +4,56 @@ function Navis() {
     const currentPage = useLocation().pathname;
 
     return (
-      <>
-          <header class="background-tint">
-          <h1 className= "title">Renard Bien-Aime</h1>
-            <nav class="nav-bar">
+        <>
+            <header class="background-tint">
+            <h1 className= "title">Renard Bien-Aime</h1>
+            <nav className="nav-bar">
                 <ul className= "nav nav-tabs">
-                  <li>
-                    <Link
-                    to= "/"
-                    className={currentPage === '/' ? 'nave-link active' : 'nav-link'}
-                    >
-                      Home
-                    </Link>
-                  </li>
-                  <li>
-                  <Link
-                    to= "/AboutMe"
-                    className={currentPage === '/AboutMe' ? 'nave-link active' : 'nav-link'}
-                    >
-                      About Me
-                    </Link>
-                  </li>
-                  <li> 
-                    <a href="#">Work</a>
-                  </li>
-                  <li> 
-                    <a href="#">Contact Me</a>
-                  </li>
-                  <li> 
-                    <a href="#" >Resume</a>
-                  </li>
+                    <li>
+                        <Link
+                        to= "/"
+                        className={currentPage === '/' ? 'nave-link active' : 'nav-link'}
+                        >
+                        Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                        to= "/AboutMe"
+                        className={currentPage === '/AboutMe' ? 'nave-link active' : 'nav-link'}
+                        >
+                        About Me
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                        to= "/Portfolio"
+                        className={currentPage === '/Portfolio' ? 'nave-link active' : 'nav-link'}
+                        >
+                        Portfolio
+                        </Link> 
+                    </li>
+                    <li>
+                        <Link
+                        to= "/Contact"
+                        className={currentPage === '/Contact' ? 'nave-link active' : 'nav-link'}
+                        >
+                        Contact
+                        </Link> 
+                    </li>
+                    <li>
+                        <Link
+                        to= "/Resume"
+                        className={currentPage === '/Resume' ? 'nave-link active' : 'nav-link'}
+                        >
+                        Resume
+                        </Link> 
+                    </li>
                 </ul>                   
             </nav>
-          </header>
-  
-      </>
+            </header>
+        </>
     )
-  }
-  
-  export default Navis
+}
+
+export default Navis
